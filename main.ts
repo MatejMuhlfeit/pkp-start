e7acd39b8d296f22ed6580eaff35383b33b6e5e6
MatejMuhlfeit <115697262+MatejMuhlfeit@users.noreply.github.com>
2023-02-16 11:31:31 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,12 @@
 let time1 = 0
 let time2 = 0
 let done = 0
-let start = 0
 RunComp.SetLightLevel()
 radio.setGroup(1)
 radio.setFrequencyBand(20)
 radio.setTransmitPower(5)
 
-input.onButtonPressed(Button.A, function() {
+input.onButtonPressed(Button.A, function () {
     done = 1
     radio.sendNumber(1)
     music.playTone(Note.C, music.beat(BeatFraction.Whole))
@@ -15,31 +14,42 @@ input.onButtonPressed(Button.A, function() {
     music.playTone(Note.C, music.beat(BeatFraction.Whole))
     basic.pause(1000)
     music.playTone(Note.A, music.beat(BeatFraction.Whole))
-    start = 1
 })
 
-input.onButtonPressed(Button.B, function() {
-    basic.showNumber(time2 - time1)
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(Math.round((time2 - time1)/ 100))
 })
 
-input.onButtonPressed(Button.AB, function() {
-    
-})
-
-
-radio.onReceivedNumber(function(receivedNumber: number) {
+input.onButtonPressed(Button.AB, function () {
+    time1 = 0
+    time2 = 0
     done = 0
 })
 
-RunComp.OnLightDrop(function() {
+
+radio.onReceivedNumber(function (receivedNumber: number) {
+    if (receivedNumber === 1) {
+        done = 0
+    }
+    else {
+        time1 = receivedNumber
+    }
+
+
+})
+
+radio.onReceivedString(function (receivedString: string) {
+    radio.sendNumber(time1)
+})
+
+RunComp.OnLightDrop(function () {
     music.playTone(Note.C, music.beat(BeatFraction.Whole))
-    if(done === 1)
-    {
+    if (done === 1) {
         time1 = control.millis()
     }
-    else if(done === 0)
-    {
+    else if (done === 0) {
         time2 = control.millis()
+        radio.sendString("1")
     }
 
 })
